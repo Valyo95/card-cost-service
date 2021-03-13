94dd0cebb9948f3a29b3812dd5e22922fc3ea136
Valyo95 <valyo95@abv.bg>
2021-03-13 19:40:06 +0200

    Initial commit

---
+++ src/main/java/com/valyo95/microservices/cardcostservice/CardCostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.valyo95.microservices.cardcostservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardCostServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardCostServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/valyo95/microservices/cardcostservice/CardCostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valyo95.microservices.cardcostservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardCostServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
