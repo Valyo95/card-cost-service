45a679d44b139741847ef54c11d9fb1e32c0abd9
Valyo95 <valyo95@abv.bg>
2021-03-15 16:29:46 +0200

    Updated the create_schema migration

--- src/main/resources/db/migration/V1.0__create_schema.sql
+++ src/main/resources/db/migration/V1.0__create_schema.sql
@@ -1,4 +1,5 @@
-CREATE TABLE country_clearing_cost(
+Drop Table IF EXISTS country_clearing_cost;
+CREATE TABLE country_clearing_cost (
       country_code varchar(2) not null,
       cost decimal(19,2),
       primary key (country_code)
